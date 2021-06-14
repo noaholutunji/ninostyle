c7b3dfd76b1eaf2076d4ac626c71e72aab831ff9
Noah Osuolale <osuolale49@gmail.com>
2021-06-14 08:35:53 +0100

    code cleanup

--- routers/user.js
+++ routers/user.js
@@ -26,18 +26,4 @@ router.post('/login', async (req, res) => {
     }
 })
 
-// router.post('/users/logout', auth, async (req, res) => {
-//     try {
-//         req.user.tokens = req.user.tokens.filter((token) => {
-//             return token.token !== req.token
-//         })
-//         await req.user.save()
-
-//         res.send()
-//     } catch (e) {
-//         res.status(500).send()
-//     }
-// })
-
-
 module.exports = router
